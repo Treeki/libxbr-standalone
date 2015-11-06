3835e97d2f991b867a9764fa6ace730d25e4a19e
Treeki <treeki@gmail.com>
2015-11-06 01:47:12 +0100

    tweak xBR algo's handling of alpha transparency
    The xBR algorithm supports alpha-transparent bitmaps, but its edge
    detection logic does not take transparency into account - so ARGB
    0x00000000 (transparent) and ARGB 0xFF000000 (opaque black) are treated
    as part of the same 'region'. This change tweaks this behaviour so that
    differences in alpha value are also considered.

--- xbr.c
+++ xbr.c
@@ -48,7 +48,8 @@ static uint32_t pixel_diff(uint32_t x, uint32_t y, const uint32_t *r2y)
     uint32_t yuv1 = r2y[x & 0xffffff];
     uint32_t yuv2 = r2y[y & 0xffffff];
 
-    return (abs((yuv1 & YMASK) - (yuv2 & YMASK)) >> 16) +
+    return (abs((x >> 24) - (y >> 24))) +
+           (abs((yuv1 & YMASK) - (yuv2 & YMASK)) >> 16) +
            (abs((yuv1 & UMASK) - (yuv2 & UMASK)) >>  8) +
            abs((yuv1 & VMASK) - (yuv2 & VMASK));
 }
